4fffc388b70f63a9de5fd7777c5af556726cce31
noahzhy <noahzhy@gmail.com>
2021-05-23 00:51:56 +0900

    add

--- hw3/model.py
+++ hw3/model.py
@@ -1,5 +1,6 @@
 import os
 import PIL
+import pathlib
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -9,16 +10,135 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 
+batch_size = 32
+img_height = 180
+img_width = 180
+
+
 def download_dataset():
-    import pathlib
     dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
     data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
     data_dir = pathlib.Path(data_dir)
 
     image_count = len(list(data_dir.glob('*/*.jpg')))
     print(image_count)
+    return data_dir
 
+
+def data_preprocessing(data_dir):
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size
+    )
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size
+    )
+    class_names = train_ds.class_names
+    print(class_names)
+
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+    image_batch, labels_batch = next(iter(normalized_ds))
+    first_image = image_batch[0]
+
+    print(np.min(first_image), np.max(first_image))
+
+    return normalized_ds, val_ds
+
+
+def build_model(num_classes=5):
+    model = Sequential([
+        layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.2),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+    model.summary()
+    return model
+
+
+def visualize_results(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+
+def predict():
+    sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
+    sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
+
+    img = keras.preprocessing.image.load_img(
+        sunflower_path, target_size=(img_height, img_width)
+    )
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+
+    print(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+    )
+
+
+def train(train_ds, val_ds, epochs=15):
+    model = build_model()
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs
+    )
+    return history
 
 
 if __name__ == "__main__":
-    download_dataset()
+    data_path = download_dataset()
+    train_ds, val_ds = data_preprocessing(data_path)
+    results = train(train_ds, val_ds, 15)
+    visualize_results(results)
+    predict()
